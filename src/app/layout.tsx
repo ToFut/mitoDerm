3bc3905d2fef2b4ba782c7605ec901a94da115b3
Segev <segev@futurix.com>
2025-08-13 10:36:18 -0400

    Fix root layout: Add missing html and body tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return children;
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
 } 
